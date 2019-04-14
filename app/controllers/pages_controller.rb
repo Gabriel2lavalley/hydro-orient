5f3cb45502decdf5b04c0eb91040eefc438bb67e
sanket navadiya <sanknavadiya162@gmail.com>
2019-04-14 20:31:43 +0530

    devise added for user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
-
+	before_action :authenticate_user!, only: [:load_map]
   def index
   end 
+
   def log_in
   end
+
   def load_map
   	
   end
+
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  authenticated :user do
+    root 'pages#load_map', as: :authenticated_root
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get 'pages/log-in'
