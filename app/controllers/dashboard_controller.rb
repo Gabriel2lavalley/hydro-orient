a2154d6e795895fa578bdf90fa0458ca7cf03a03
gabriel2lavalley <gabrieldepaz69@gmail.com>
2019-07-28 10:21:48 -0400

    m3/s

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,23 +7,22 @@ class DashboardController < ApplicationController
       renderAt: "chartContainer",
       dataSource: {
         chart: {
-        caption: "Comparison of Quarterly Revenue",
-        subCaption: "Harry's SuperMart",
+        caption: "Comparison between this year and last year",
         xAxisname: "Quarter",
-        yAxisName: "Amount ($)",
-        numberPrefix: "$",
+        yAxisName: "m3",
+        numberPrefix: "",
         theme: "fint",
         exportEnabled: "1",
         },
         categories: [{
           category: [
-            { label: "Q1" },
-            { label: "Q2" },
-            { label: "Q3" },
-            { label: "Q4" },
-            { label: "Q5" },
-            { label: "Q6" },
-            { label: "Q7" }
+            { label: "Q1 2018" },
+            { label: "Q2 2018" },
+            { label: "Q3 2018" },
+            { label: "Q4 2018" },
+            { label: "Q1 2019" },
+            { label: "Q2 2019" },
+            { label: "Q3 2019" }
           ]
         }],
         dataset: [
@@ -67,11 +66,11 @@ class DashboardController < ApplicationController
           renderAt: 'chartContainer',
           dataSource: {
             :"chart" => {
-              :"caption" => 'Monthly water supply among four routers in L/s',
-              :"yaxisname" => 'Water Supply Speed in L/s',
+              :"caption" => 'Monthly water supply among four routers in m3/h',
+              :"yaxisname" => 'Water Supply Speed in m3/h',
               :"subcaption" => 'Jan-Jul',
               :"showhovereffect" => '1',
-              :"numbersuffix" => 'L/s',
+              :"numbersuffix" => ' m3/h',
               :"drawcrossline" => '1',
               :"plottooltext" =>
                 '<b>$dataValue</b> of water supply were on $seriesName',
@@ -94,37 +93,37 @@ class DashboardController < ApplicationController
               {
                 :"seriesname" => 'Router 1',
                 :"data" => [
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) }
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) }
                 ]
               },
               {
                 :"seriesname" => 'Router 2',
                 :"data" => [
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) }
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) }
                 ]
               },
               {
                 :"seriesname" => 'Router 3',
                 :"data" => [
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) },
-                  { :"value" => Random.rand(100) }
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) },
+                  { :"value" => Random.rand(1000) }
                 ]
               },
               {
@@ -156,26 +155,26 @@ class DashboardController < ApplicationController
           dataSource: {
             :"chart" => {
               :"caption" => 'Average Speed Of Water Supply',
-              :"yaxisname" => 'Speed (in L/s)',
+              :"yaxisname" => 'Speed (in m3/h)',
               :"subcaption" => '[2008-2019]',
-              :"numbersuffix" => ' L/s',
+              :"numbersuffix" => ' m3/h',
               :"rotatelabels" => '1',
               :"setadaptiveymin" => '1',
               :"theme" => 'fusion'
             },
             :"data" => [
-              { :"label" => '2008', :"value" => Random.rand(0.1...99.99) },
-              { :"label" => '2009', :"value" => Random.rand(0.1...99.99) },
-              { :"label" => '2010', :"value" => Random.rand(0.1...99.99) },
-              { :"label" => '2011', :"value" => Random.rand(0.1...99.99) },
-              { :"label" => '2012', :"value" => Random.rand(0.1...99.99)},
-              { :"label" => '2013', :"value" => Random.rand(0.1...99.99) },
-              { :"label" => '2014', :"value" => Random.rand(0.1...99.99) },
-              { :"label" => '2015', :"value" => Random.rand(0.1...99.99) },
-              { :"label" => '2016', :"value" => Random.rand(0.1...99.99)},
-              { :"label" => '2017', :"value" => Random.rand(0.1...99.99) },
-              { :"label" => '2018', :"value" => Random.rand(0.1...99.99) },
-              { :"label" => '2019', :"value" => Random.rand(0.1...99.99) },
+              { :"label" => '2008', :"value" => Random.rand(300...1200) },
+              { :"label" => '2009', :"value" => Random.rand(300...1200) },
+              { :"label" => '2010', :"value" => Random.rand(300...1200) },
+              { :"label" => '2011', :"value" => Random.rand(300...1200) },
+              { :"label" => '2012', :"value" => Random.rand(300...1200)},
+              { :"label" => '2013', :"value" => Random.rand(300...1200) },
+              { :"label" => '2014', :"value" => Random.rand(300...1200) },
+              { :"label" => '2015', :"value" => Random.rand(300...1200) },
+              { :"label" => '2016', :"value" => Random.rand(300...1200)},
+              { :"label" => '2017', :"value" => Random.rand(300...1200) },
+              { :"label" => '2018', :"value" => Random.rand(300...1200) },
+              { :"label" => '2019', :"value" => Random.rand(300...1200) },
             ]
           }
         }
